5a67704ebfa3954b24b2a56444f445bb0acd5f22
sagrvma <therealsagarverma@gmail.com>
2022-03-03 14:57:36 +0530

    Added functionality to search for desired city and display it's weather by clicking the search icon or pressing enter

--- index.js
+++ index.js
@@ -17,12 +17,25 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".temp").innerText = temp + "° C";
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed : " + speed + " km/h";
-    }
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
