4c8c0e011860f7cf73484b7d186c399eca9ff688
sagrvma <therealsagarverma@gmail.com>
2022-03-03 14:38:27 +0530

    Initial script to display results fetched by the API

---
+++ index.js
@@ -0,0 +1,28 @@
+let weather = {
+
+    apiKey: "8234c4491ac02210ffaa818e06ec40f3",
+
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+    },
+
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "° C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed : " + speed + " km/h";
+    }
+};
